cc8de898a42ac14bf776665cff0ec16892276b14
152334H <54623771+152334H@users.noreply.github.com>
2022-01-06 13:54:12 +0800

    part1c

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,40 +1,21 @@
 //import logo from './logo.svg';
 //import './App.css';
 
-import React from 'react'
+import React, {useState} from 'react'
 
-const Header = (props) => (<h1>{props.course}</h1>);
-const Part = (props) => (<p>{props.part} {props.exercises}</p>);
-const Content = (props) => (<div>
-    {props.parts.map(part =>
-        <Part part={part.name} exercises={part.exercises}/>
-    )}
-    </div>);
-const Total = (props) => (<p>
-    Number of exercises {props.parts.map(p => p.exercises)
-            .reduce((a,b) => a+b)}
-    </p>);
+const Display = ({counter}) => <div>{counter}</div>;
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>;  
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [{
-            name: 'Fundamentals of React',
-            exercises: 10
-        }, {
-            name: 'Using props to pass data',
-            exercises: 7
-        }, {
-            name: 'State of a component',
-            exercises: 14
-        }]
-    };
-    return (
-        <div>
-        <Header course={course.name}/>
-        <Content parts={course.parts}/>
-        <Total parts={course.parts}/>
-        </div>
-    )
+    const [counter, setCounter] = useState(0);
+    const increment = () => setCounter(counter + 1);
+    const decrement = () => setCounter(counter - 1);
+    return (<>
+        <Display counter={counter}/>
+        <Button onClick={increment} text="increase"/>
+        <Button onClick={decrement} text="decrease"/>
+        <Button onClick={() => setCounter(0)} text="reset"/>
+
+        </>)
 }
 
 export default App;
