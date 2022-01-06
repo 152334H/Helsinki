9a8463cc0da2f6bf7e6eea275bc973250beb8c50
152334H <54623771+152334H@users.noreply.github.com>
2022-01-06 14:37:05 +0800

    part1d_preex

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,20 +2,39 @@
 //import './App.css';
 
 import React, {useState} from 'react'
-
-const Display = ({counter}) => <div>{counter}</div>;
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>;  
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return <div>the app is used by pressing the buttons</div>
+    }
+    return <div>button press history: {props.allClicks.join(' ')}</div>
+}
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
 const App = () => {
-    const [counter, setCounter] = useState(0);
-    const increment = () => setCounter(counter + 1);
-    const decrement = () => setCounter(counter - 1);
-    return (<>
-        <Display counter={counter}/>
-        <Button onClick={increment} text="increase"/>
-        <Button onClick={decrement} text="decrease"/>
-        <Button onClick={() => setCounter(0)} text="reset"/>
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
-        </>)
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
+
+    return (
+        <div>
+        {left}
+        <Button handleClick={handleLeftClick} text="left"/>
+        <Button handleClick={handleRightClick} text="right"/>
+        {right}
+        <History allClicks={allClicks}/>
+        </div>
+    )
 }
 
 export default App;
