8628ce981711d53f5cf7f3b05523f02e8bd0d875
152334H <54623771+152334H@users.noreply.github.com>
2022-01-08 19:01:15 +0800

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -11,10 +11,16 @@ const Numbers = ({persons}) => (<>
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1234567' }
-    ]) 
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [nameFilter, setNameFilter] = useState('')
+
+    const shownPersons = persons.filter(p => p.name.toLowerCase().includes(nameFilter.toLowerCase()))
 
     const addName = e => {
         e.preventDefault()
@@ -29,10 +35,13 @@ const App = () => {
         }
         setPersons(persons.concat(newPerson))
         setNewName('')
+        setNewNumber('')
     }
 
     return (<div>
         <h2>Phonebook</h2>
+        <p>filter shown with <input value={nameFilter} onChange={e => setNameFilter(e.target.value)}/></p>
+        <h2>add a new</h2>
         <form onSubmit={addName}>
             <div>name: <input value={newName} onChange={e => setNewName(e.target.value)}/></div>
             <div>number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}/></div>
@@ -40,7 +49,7 @@ const App = () => {
                 <button type="submit">add</button>
             </div>
         </form>
-        <Numbers persons={persons} />
+        <Numbers persons={shownPersons} />
     </div>)
 }
 
