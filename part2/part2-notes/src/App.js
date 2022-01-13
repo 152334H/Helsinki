e9fce2a5bc1e72f1b9a32f2918aeb484e0e2964e
152334H <54623771+152334H@users.noreply.github.com>
2022-01-13 10:05:09 +0800

    2e_preex

--- part2/part2-notes/src/App.js
+++ part2/part2-notes/src/App.js
@@ -1,14 +1,31 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 
 import Note from './components/Note'
 import noteService from './services/notes'
 
-const DB_URL = 'http://de.irscybersec.tk:3001/'
+const Notification = ({ message }) => {
+    if (message === null) { return null }
+    return (<div className="error">
+        {message}
+    </div>)
+}
+
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic',
+        fontSize: 16
+    }
+    return (<div style={footerStyle}>
+        <br/>
+        <em>Note app, Dept of CS, Noose 2021 </em>
+    </div>)
+}
 const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('a new note...')
     const [showAll, setShowAll] = useState(true)
+    const [errMsg, setErrMsg] = useState(null)
 
     useEffect(() => noteService.getAll()
             .then(notes => setNotes(notes))
@@ -22,7 +39,10 @@ const App = () => {
             .then(returnedNote => setNotes(notes.map(
                 note => note.id !== id ? note : returnedNote
             ))).catch(error => {
-                alert(`the note '${note.content}' was already deleted from server`)
+                setErrMsg(`Note '${note.content}' was already removed from server`)
+                setTimeout(() => {
+                    setErrMsg(null)
+                }, 5000)
                 setNotes(notes.filter(n => n.id !== id))
             })
     }
@@ -49,6 +69,7 @@ const App = () => {
 
     return (<div>
         <h1>Notes</h1>
+        <Notification message={errMsg} />
         <div>
             <button onClick={() => setShowAll(!showAll)}>
                 show {showAll ? 'important' : 'all'}
@@ -64,6 +85,7 @@ const App = () => {
             <input value={newNote} onChange={handleNoteChange}/>
             <button type="submit">add</button>
         </form>
+        <Footer />
     </div>)
 }
 
